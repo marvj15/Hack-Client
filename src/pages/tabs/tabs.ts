6d8c2a3f3abf2ca0440e942622379fd26d2eabe5
Jon Marvin <jonmarvin2015@gmail.com>
2018-10-13 01:38:02 -0500

    Created Profile Tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,6 +4,7 @@ import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { EventPage } from '../event/event';
+import { ProfilePage } from '../profile/profile';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +14,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
-  tab4Root = EventPage;
+  tab4Root = ProfilePage;
 
   constructor() {
 
