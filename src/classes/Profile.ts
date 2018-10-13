4287a7374a720ba96ab0c2424a67cf9b77d2778e
Jon Marvin <jonmarvin2015@gmail.com>
2018-10-13 01:29:47 -0500

    created Profile class

--- src/classes/Profile.ts
+++ src/classes/Profile.ts
@@ -1,10 +1,31 @@
+
 class Profile{
 
+    key: number;
     username: String;
+    fName: String;
+    lName: String;
+    email: String;
+    bio: String;
+    dob: String;
+    gender: String;
+    lang: String;
+    city: String;
+    state: String;
 
-
-    constructor (username: String){
+    constructor (key: number,username: String, fName: String, lName: String, email: String, bio: String, dob: String, gender: String, lang: String, city: String, state: String){
+        this.key = key;
         this.username = username;
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.bio = bio;
+        this.dob = dob;
+        this.gender = gender;
+        this.lang = lang;
+        this.city = city;
+        this.state = state;
+
     }
 
 }
